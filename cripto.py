7276b13271913c34f5b4a577ed878e54acd62fde
Allainn Christiam <allainn@alunos.utfpr.edu.br>
2020-10-20 22:21:33 -0300

    Alter menu usage/add save file

--- cripto.py
+++ cripto.py
@@ -1,19 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#!/usr/bin/python3
 """
-Algoritmo responsavel por criptografar ou descriptografar
-textos ou arquivos e gerar um arquivo de saída.
+Este Componente abre um arquivo em formato .csv, intitulado 
+pckgCount. Este arquivo possui a série temporal referente à 
+contagem dos pacotes trafegados por cada IP em cada intervalo 
+de tempo. Ainda é responsável por agrupar os IPs com 
+características semelhantes (quantidade de pacotes trafegados), 
+com o objetivo de reduzir o conjunto de IPs a serem analisados 
+e salva os dados em um novo arquivo com o mesmo nome do 
+argumento de entrada.
 
-Autor: Allainn Christiam J. Tavares
+Este componente é chamado diretamente no terminal ou por meio do 
+Shell Script passando como parâmetro o nome do arquivo .csv 
+contendo a quantidade de pacotes, segue o seguinte exemplo:
+
+.. code-block:: shell
+    
+    python3 clustering.py pckgCount.csv
+"""
+"""
+Este componente é responsável por criptografar ou descriptografar 
+textos inseridos diretamente pelo terminal e retornar o texto no
+mesmo. Além disso é possível realizar a criptografia ou
+descriptografia de um arquivo, a saída é armazenada em um arquivo
+txt com o nome de criptografia ou descriptografia.
+
+Autores: Allainn Christiam J. Tavares
+         Marcos Alexandre dos Anjos       
 Data Criação: 25/02/2020
-Versão: 0.1.0
+Versão: 0.1.2
+
+Este componente é chamado diretamente no terminal ou por meio do 
+Shell Script passando como parâmetro a opção de criptografar ou
+descriptografar e a opção se o método escolhido será aplicado em 
+um arquivo ou texto, acompanhado pelo nome do arquivo ou o texto.
+
+Exemplos:
+
+.. code-block:: shell
+    
+    $ python3 cripto.py -c -t peso
+
+.. code-block:: shell
+    
+    $ python3 cripto.py -d -t hpuU
+
+.. code-block:: shell
+    
+    $ python3 cripto.py -c -t peso
+
+.. code-block:: shell
+    
+    $ python3 cripto.py -c -t peso
 """
 import sys
 import math
+import argparse
 
-VERSAO='0.1.0'
+VERSAO='0.1.2'
 
+"""
+
+"""
 def Fibonacci(n): 
     if n<0: 
         print("Incorrect input") 
@@ -36,11 +85,12 @@ def print_error():
 
 def Criptografar(text):
     """
-    Método responsável em realizar a criptografoa
+    Método responsável em realizar a criptografia
 
     :param text: Texto a ser criptografado
     """
     tam=len(text)
+    text = text[::-1]
     x=math.sqrt(tam)
     if x>int(x):
         num=int(x)+1
@@ -119,75 +169,72 @@ def Descriptografar(text):
             if fib > 15:
                 fib = 3
             
-    oldText=oldText.rstrip(' ')
+    oldText=oldText.rstrip(' ')[::-1]
     print("Descriptografado com sucesso")
     return oldText
 
 def main():
-    txt_help='''Uso: python3 cripto.py OPÇÃO(-c | -d) [-a ARQUIVO | -t TEXTO]
-    Criptografa ou Descriptografa um ARQUIVO ou TEXTO.
+    parser = argparse.ArgumentParser(description="Criptografa ou Descriptografa um ARQUIVO ou TEXTO.", 
+                                    add_help=False)
 
-    -c, --cripto	criptografar
-    -d, --decripto	descriptografar
+    optional = parser.add_argument_group('Argumentos')
+    optional.add_argument('-c', '--cripto', action='store_true',
+                        help="Criptografar")
+    optional.add_argument('-d', '--decripto', action='store_true',
+                        help="Descriptografar")
 
-    -t, --texto		entrada de texto
-    -a, --arquivo	entrada de arquivo
+    optional.add_argument('-t', '--texto', 
+                        help="Entrada de texto")
+    optional.add_argument('-a', '--abrir', metavar='ARQUIVO',
+                        help="Entrada de arquivo")
+    optional.add_argument('-s', '--salvar', metavar='ARQUIVO', 
+                        help="Salvar no arquivo")
 
-    -h, --help		mostra esta ajuda e sai
-    -v, --version	informa a versão e sai
+    optional.add_argument("-v", "--version", action="version",
+                        help="Exibe a versão. ", 
+                        version='Cripto - '+VERSAO)
+    optional.add_argument('-h', '--help', action='help', 
+                        default=argparse.SUPPRESS,
+                        help='Mostra esta mensagem.')
 
-    Exemplo:
-        python3 cripto -c -t Teste
-        python3 cripto -d -a criptografado.txt
+    args = parser.parse_args()
 
-    Autor: Allainn Christiam'''
-
-
-    try:
-        param = sys.argv[1]
-        if param == '-h' or param == '--help':
-            print(txt_help)
-        elif param == '-v' or param == '--version':
-            print('Cripto 0.0.2')
-        elif param=='-c' or param=='--cripto' or param=='-d' or param=='--decripto':
+    def verificar(func):
+        if args.texto and not(args.abrir):
+            print(args.texto)
+            return func(args.texto)
+        elif not(args.texto) and args.abrir:
             try:
-                param2 = sys.argv[2]
-                if param2=='-t' or param2=='--texto':
-                    try:
-                        text = sys.argv[3]
-                        text = sys.argv[3:]
-                        text = ' '.join(text)
-                        if param=='-c' or param=='--cripto':
-                            text = Criptografar(text)
-                        elif param=='-d' or param=='--decripto':
-                            text = Descriptografar(text)
-                        print(text)
-                    except IndexError:
-                        print_error()
-                elif param2=='-a' or param2=='--arquivo':
-                    try:
-                        nome_arq = sys.argv[3]
-                        try:
-                            arq = open(nome_arq, 'r')
-                        except FileNotFoundError:
-                            print('Arquivo ou diretório não encontrado!!')
-                            quit()
-                        text=arq.read()
-                        arq.close()
-                        if param=='-c' or param=='--cripto':
-                            text = Criptografar(text)
-                            arq = open('criptografado.txt', 'w')
-                        elif param=='-d' or param=='--decripto':
-                            text = Descriptografar(text)
-                            arq = open('descriptografado.txt', 'w')
-                        arq.write(text)
-                        arq.close()
-                    except IndexError:
-                        print_error()
-            except IndexError:
-                print_error()
-    except IndexError:
-        print_error()
+                arq = open(args.abrir, 'r')
+            except FileNotFoundError:
+                print('Arquivo ou diretório não encontrado!!')
+                quit()
+            text=arq.read()
+            arq.close()
+            return func(text)
+        else:
+            print("Error!")
+            print("Não foi passado o argumento de texto ou "
+                +"arquivo, ou foi passado ambos os argumentos!")
+            quit()
+
+    if args.cripto and not(args.decripto):
+        text = verificar(Criptografar)
+    elif args.decripto and not(args.cripto):
+        text = verificar(Descriptografar)
+    else:
+        print("Error!")
+        print("Não foi passado o argumento de criptografia ou "
+            +"descriptografia, ou foi passado ambos os argumentos!")
+        quit()
+
+    if args.salvar:
+        arq = open(args.salvar, 'w')
+        arq.write(text)
+        arq.close()
+        print("Salvo no arquivo " + args.salvar) 
+    else:
+        print(text)
 
 if __name__ == "__main__":
     main()
